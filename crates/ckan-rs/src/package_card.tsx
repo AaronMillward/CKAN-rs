f7756aac025262745fc32cd18ae8103611e4700d
AaronMillward <aaronmillward1999@gmail.com>
2023-09-20 20:37:34 +0100

    Add install button to package_card

--- crates/ckan-rs/src/package_card.tsx
+++ crates/ckan-rs/src/package_card.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import './styles.css';
 import { invoke } from '@tauri-apps/api'
 
 export default function PackageCard({pack}: any) {
-	function click_handler() {
+	const [selected, setSelected] = useState(false);
+
+	function click_handler_info() {
 		invoke("open_package_detail_window", { package: pack } );
 	}
 
+	function click_handler_install() {
+		setSelected(!selected)
+	}
+
+	let controls = selected ? 
+		<div className="card-right package-controls package-controls-uninstall" onClick={click_handler_install}/>
+		:
+		<div className="card-right package-controls package-controls-install" onClick={click_handler_install}/>
+		;
+
 	return (
-		<div className="card package-card" onClick={click_handler}>
-			<h2 className="title">{pack.name}</h2>
-			<span className="authors">{pack.author.join(", ")}</span>
-			<span className="version">{pack.identifier.version.version}</span>
+		<div className='package-card-container'>
+			<div className="card package-card card-left" onClick={click_handler_info}>
+				<h2 className="title">{pack.name}</h2>
+				<span className="authors">{pack.author.join(", ")}</span>
+				<span className="version">{pack.identifier.version.version}</span>
+			</div>
+			{controls}
 		</div>
 	)
 }
